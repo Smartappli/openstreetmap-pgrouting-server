5eddf3930e154b5f127dbe4dd1c338b510c2fa73
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2021-06-13 19:38:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 set -x
 
 function createPostgresConfig() {
-  cp /etc/postgresql/12/main/postgresql.custom.conf.tmpl /etc/postgresql/12/main/conf.d/postgresql.custom.conf
-  sudo -u postgres echo "autovacuum = $AUTOVACUUM" >> /etc/postgresql/12/main/conf.d/postgresql.custom.conf
-  cat /etc/postgresql/12/main/conf.d/postgresql.custom.conf
+  cp /etc/postgresql/13/main/postgresql.custom.conf.tmpl /etc/postgresql/13/main/conf.d/postgresql.custom.conf
+  sudo -u postgres echo "autovacuum = $AUTOVACUUM" >> /etc/postgresql/13/main/conf.d/postgresql.custom.conf
+  cat /etc/postgresql/13/main/conf.d/postgresql.custom.conf
 }
 
 function setPostgresPassword() {
@@ -16,18 +16,15 @@ if [ "$#" -ne 1 ]; then
     echo "usage: <import|run>"
     echo "commands:"
     echo "    import: Set up the database and import /data.osm.pbf"
-    echo "    run: Runs Apache and renderd to serve tiles at /tile/{z}/{x}/{y}.png"
-    echo "environment variables:"
-    echo "    THREADS: defines number of threads used for importing / tile rendering"
-    echo "    UPDATES: consecutive updates (enabled/disabled)"
+    echo "    run: Runs PG Routing service"
     exit 1
 fi
 
 if [ "$1" = "import" ]; then
     # Ensure that database directory is in right state
     chown postgres:postgres -R /var/lib/postgresql
-    if [ ! -f /var/lib/postgresql/12/main/PG_VERSION ]; then
-        sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /var/lib/postgresql/12/main/ initdb -o "--locale C.UTF-8"
+    if [ ! -f /var/lib/postgresql/13/main/PG_VERSION ]; then
+        sudo -u postgres /usr/lib/postgresql/13/bin/pg_ctl -D /var/lib/postgresql/13/main/ initdb -o "--locale C.UTF-8"
     fi
 
     # Initialize PostgreSQL
@@ -48,20 +45,16 @@ if [ "$1" = "import" ]; then
     # Download Luxembourg as sample if no data is provided
     if [ ! -f /data.osm.pbf ] && [ -z "$DOWNLOAD_PBF" ]; then
         echo "WARNING: No import file at /data.osm.pbf, so importing Luxembourg as example..."
-        DOWNLOAD_PBF="https://download.geofabrik.de/europe/luxembourg-latest.osm.pbf"
-        DOWNLOAD_POLY="https://download.geofabrik.de/europe/luxembourg.poly"
+        DOWNLOAD_PBF="https://download.geofabrik.de/europe/belgium-latest.osm.pbf"
     fi
 
     if [ -n "$DOWNLOAD_PBF" ]; then
         echo "INFO: Download PBF file: $DOWNLOAD_PBF"
         wget "$WGET_ARGS" "$DOWNLOAD_PBF" -O /data.osm.pbf
-        if [ -n "$DOWNLOAD_POLY" ]; then
-            echo "INFO: Download PBF-POLY file: $DOWNLOAD_POLY"
-            wget "$WGET_ARGS" "$DOWNLOAD_POLY" -O /data.poly
-        fi
     fi
     
     # Import data
+    sudo -u routing osm2pgrouting --f data.osm.pbf --conf mapconfig.xml  --dbname routing --clean    
     sudo -u routing osm2pgrouting --f data.osm.pbf --conf mapconfig_for_bicycles.xml  --dbname bicycles_routing --clean
     sudo -u routing osm2pgrouting --f data.osm.pbf --conf mapconfig_for_cars.xml --dbname cars_routing --clean
     sudo -u routing osm2pgrouting --f data.osm.pbf --conf mapconfig_for_pedestrian.xml  --dbname pedestrian_routing --clean
