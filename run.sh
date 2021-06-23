fb2148d32ff2eddbd225fdaf33d4ca4d0c7dfd36
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2021-06-23 02:22:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -57,12 +57,12 @@ if [ "$1" = "import" ]; then
     fi
     
     # Import data
-    sudo -u routing osmconvert data.osm.pbf --drop-author --drop-version --out-pbf -o=output_data_reduc.osm.pbf
+    sudo -u routing osmconvert data.osm.pbf --drop-author --drop-version --out-osmf -o=output_data_reduc.osm
     rm data.osm.pbf
-    sudo -u routing osm2pgrouting --f output_data_reduc.osm.pbf --conf mapconfig.xml  --dbname routing --clean    
-    sudo -u routing osm2pgrouting --f output_data_reduc.osm.pbf --conf mapconfig_for_bicycles.xml  --dbname bicycles_routing --clean
-    sudo -u routing osm2pgrouting --f output_data_reduc.osm.pbf --conf mapconfig_for_cars.xml --dbname cars_routing --clean
-    sudo -u routing osm2pgrouting --f output_data_reduc.osm.pbf --conf mapconfig_for_pedestrian.xml  --dbname pedestrian_routing --clean
+    sudo -u routing osm2pgrouting --f output_data_reduc.osm --conf mapconfig.xml  --dbname routing --clean    
+    sudo -u routing osm2pgrouting --f output_data_reduc.osm --conf mapconfig_for_bicycles.xml  --dbname bicycles_routing --clean
+    sudo -u routing osm2pgrouting --f output_data_reduc.osm --conf mapconfig_for_cars.xml --dbname cars_routing --clean
+    sudo -u routing osm2pgrouting --f output_data_reduc.osm --conf mapconfig_for_pedestrian.xml  --dbname pedestrian_routing --clean
     
     # Create indexes
     #sudo -u postgres psql -d routing -f indexes.sql
